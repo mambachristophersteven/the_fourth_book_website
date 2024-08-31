560252d61d78ad57ec07330a29c525b06a3d667c
Christopher Steven <christopherstevennunoo@gmail.com>
2024-08-31 22:52:29 +0000

    js addex

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const menuButton = document.getElementById('menu-btn');
+const closeButton = document.getElementById('close-btn');
+const activeMenu = document.getElementById('mobile-menu-view');
 
+menuButton.addEventListener('click',()=>{
+    activeMenu.style.display= "flex";
+})
 
-console.log('helo');
+closeButton.addEventListener('click',()=>{
+    activeMenu.style.display= "none";
+})
