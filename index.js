4867c226b516c34c8550083071267bc0dd851874
Christopher Steven <christopherstevennunoo@gmail.com>
2024-09-01 21:32:43 +0000

    email connected successfully

--- index.js
+++ index.js
@@ -35,23 +35,3 @@ mobileHome.addEventListener('click',()=>{
     activeMenu.style.display= "none";
 })
 
-function SendMessage(){
-    (function(){
-        emailjs.init("jnY9MMHZ-9EO2xtvF"); //Account public key
-    })();
-
-    var serviceID = "service_qqdqf35";  //Email service ID
-    var templateID = "template_9zitqgb"; //Email Template ID
-
-    var params = {
-        senderName: document.querySelector("#name").value,
-        senderMail: document.querySelector("#email").value,
-        senderPhone: document.querySelector("#telephone").value,
-        senderTopic: document.querySelector("#topic").value,
-        senderMessage: document.querySelector("#message").value,
-    };
-    emailjs.send(serviceID, templateID, params)
-    .then( res=> {
-        alert('We have received your message.' + params['senderName'] + '!');
-    })
-}
